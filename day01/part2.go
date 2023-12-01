26088f2da1477b9e190c67bb8a9fc352708f40c1
BenJuan26 <benjamin.schubert@qlik.com>
2023-12-01 16:54:06 -0500

    Day 1 part 2 WIP

--- day01/part2.go
+++ day01/part2.go
@@ -3,9 +3,38 @@ package main
 import (
 	"fmt"
 	"strconv"
+	"strings"
 
 	advent "github.com/BenJuan26/advent2022"
 )
+
+var writtenDigits = []string{
+	"one",
+	"two",
+	"three",
+	"four",
+	"five",
+	"six",
+	"seven",
+	"eight",
+	"nine",
+}
+
+func ParseDigit(chars string) int {
+	lastChar := chars[len(chars)-1]
+	digit, err := strconv.Atoi(string(lastChar))
+	if err == nil {
+		return digit
+	}
+
+	for i, writtenDigit := range writtenDigits {
+		if strings.HasSuffix(chars, writtenDigit) {
+			return i + 1
+		}
+	}
+
+	return -1
+}
 
 func Part2() {
 	lines, err := advent.ReadTestInput()
@@ -17,13 +46,18 @@ func Part2() {
 	for _, line := range lines {
 		firstDigit := -1
 		lastDigit := -1
-		for _, char := range line {
-			digit, err := strconv.Atoi(string(char))
-			if err == nil {
+		cursorStart := 0
+		cursorEnd := 1
+		for cursorEnd < len(line) {
+			digit := ParseDigit(line[cursorStart:cursorEnd])
+			cursorEnd += 1
+			if digit >= 0 {
 				if firstDigit < 0 {
 					firstDigit = digit
 				}
 				lastDigit = digit
+				cursorStart = cursorEnd + 1
+				cursorEnd += 1
 			}
 		}
 		value := firstDigit*10 + lastDigit
