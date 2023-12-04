595b87bdcbeec9d422bb97aa8a0b99e4279d6a34
BenJuan26 <benjamin.schubert@qlik.com>
2023-12-04 09:14:23 -0500

    Day 4 part 2

--- day04/part2.go
+++ day04/part2.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 
 	advent "github.com/BenJuan26/advent2023"
 )
@@ -12,5 +14,35 @@ func Part2() {
 		panic(err)
 	}
 
-	fmt.Println(lines[0])
+	total := 0
+	copies := map[int]int{}
+	for cardNum, line := range lines {
+		card := strings.Split(line, ": ")[1]
+		cardParts := strings.Split(card, " | ")
+		winningNums := map[int]bool{}
+		for _, numString := range strings.Fields(cardParts[0]) {
+			num, err := strconv.Atoi(numString)
+			if err != nil {
+				panic(err)
+			}
+			winningNums[num] = true
+		}
+		matches := 0
+		for _, numString := range strings.Fields(cardParts[1]) {
+			num, err := strconv.Atoi(numString)
+			if err != nil {
+				panic(err)
+			}
+			if winningNums[num] {
+				matches += 1
+			}
+		}
+		totalCopies := 1 + copies[cardNum]
+		for i := 0; i < matches; i++ {
+			copies[cardNum+i+1] += totalCopies
+		}
+		total += totalCopies
+	}
+
+	fmt.Println(total)
 }
