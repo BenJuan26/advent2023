4efad2f5ee8257e9c6930bf607feb3186208b404
BenJuan26 <benjamin.schubert@qlik.com>
2023-12-05 16:28:22 -0500

    Working for test but too inefficient

--- day05/part2.go
+++ day05/part2.go
@@ -8,30 +8,95 @@ import (
 	advent "github.com/BenJuan26/advent2023"
 )
 
+type SeedRange struct {
+	Start  int
+	Length int
+}
+
+// possibilities:
+// seed and map have no overlap -> add seed range verbatim
+// seed is entirely contained by map -> map of seed start to map of seed end
+// map is entirely contained by seed -> three sections: 1) start of seed to start of overlap 2) mapped overlap section 3) end of map to end of seed
+// seed and map overlap partially
+//   seed starts first -> two sections: 1) start of seed to start of map 2) start of map to end of seed
+//   map starts first -> two sections: 1) start of seed to end of map 2) end of map to end of seed
+
+func (m *Map) MapRange(seedRanges []SeedRange) []SeedRange {
+	results := []SeedRange{}
+	for i := 0; i < len(seedRanges); i++ {
+		sr := seedRanges[i]
+		matched := false
+		for _, mr := range m.Ranges {
+			// no overlap with this map range
+			if sr.Start > (mr.Source+mr.Length-1) || sr.Start+sr.Length-1 < mr.Source {
+				continue
+			}
+			matched = true
+			// seed is entirely contained by map
+			if sr.Start >= mr.Source && sr.Start+sr.Length-1 <= mr.Source+mr.Length-1 {
+				results = append(results, SeedRange{Start: mr.Destination + sr.Start - mr.Source, Length: sr.Length})
+				continue
+			}
+			// map is entirely contained by seed
+			if mr.Source >= sr.Start && mr.Source+mr.Length-1 <= sr.Start+sr.Length-1 {
+				// come back to the two unmapped parts later
+				seedRanges = append(seedRanges, SeedRange{Start: sr.Start, Length: mr.Source - sr.Start})
+				seedRanges = append(seedRanges, SeedRange{Start: mr.Source + mr.Length, Length: sr.Start + sr.Length - mr.Source + mr.Length})
+				results = append(results, SeedRange{Start: mr.Destination, Length: mr.Length})
+				continue
+			}
+			// partial overlap: seed starts first
+			if sr.Start < mr.Source && sr.Start+sr.Length < mr.Source+mr.Length {
+				seedRanges = append(seedRanges, SeedRange{Start: sr.Start, Length: mr.Source - sr.Start})
+				results = append(results, SeedRange{mr.Destination, sr.Length - (mr.Source - sr.Start)})
+				continue
+			}
+			// partial overlap: map starts first
+			if mr.Source < sr.Start && mr.Source+mr.Length < sr.Start+sr.Length {
+				seedRanges = append(seedRanges, SeedRange{Start: mr.Source + mr.Length, Length: mr.Source + mr.Length - (sr.Start + sr.Length - 1)})
+				results = append(results, SeedRange{Start: mr.Destination + sr.Start - mr.Source, Length: mr.Source + mr.Length - sr.Start})
+				continue
+			}
+			panic("shouldn't reach here")
+		}
+		if !matched {
+			results = append(results, sr)
+		}
+	}
+
+	sort.Slice(results, func(i, j int) bool {
+		return results[i].Start < results[j].Start
+	})
+
+	return results
+}
+
 func Part2() {
-	lines, err := advent.ReadInput()
+	lines, err := advent.ReadTestInput()
 	if err != nil {
 		panic(err)
 	}
 
-	seeds := []int{}
+	seedRanges := []SeedRange{}
 	seedsString := strings.Split(lines[0], ": ")[1]
 	seedsStrings := strings.Fields(seedsString)
-	for _, s := range seedsStrings {
-		seeds = append(seeds, advent.MustAtoi(s))
+	for i := 0; i < len(seedsStrings); i += 2 {
+		seedRanges = append(seedRanges, SeedRange{Start: advent.MustAtoi(seedsStrings[i]), Length: advent.MustAtoi(seedsStrings[i+1])})
 	}
 
 	maps := PopulateMaps(lines)
 
-	locations := []int{}
-	for _, seed := range seeds {
-		num := seed
-		for _, m := range maps {
-			num = m.Map(num)
-		}
-		locations = append(locations, num)
+	// locations := []SeedRange{}
+	// for _, sr := range seedRanges {
+	// 	r := []SeedRange{sr}
+	for _, m := range maps {
+		seedRanges = m.MapRange(seedRanges)
 	}
+	// locations = append(locations, r...)
+	// }
 
-	sort.Ints(locations)
-	fmt.Println(locations[0])
+	sort.Slice(seedRanges, func(i, j int) bool {
+		return seedRanges[i].Start < seedRanges[j].Start
+	})
+	fmt.Println(seedRanges[0].Start)
 }
