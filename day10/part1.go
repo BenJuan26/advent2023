a83f0d5b1bca30a2647c31226b004bf6a601e672
BenJuan26 <brm.schubert@gmail.com>
2023-12-11 18:07:05 -0500

    Day 10 part 2

--- day10/part1.go
+++ day10/part1.go
@@ -103,24 +103,41 @@ func getConnectedDirs(grid []string, y, x int) (int, int) {
 	if len(dirs) != 2 {
 		panic("too many/few dirs connected")
 	}
+
+	if dirs[0] == Up || dirs[1] == Up {
+		if dirs[0] == Left || dirs[1] == Left {
+			grid[y] = grid[y][:x] + "J" + grid[y][x+1:]
+		} else if dirs[0] == Right || dirs[1] == Right {
+			grid[y] = grid[y][:x] + "L" + grid[y][x+1:]
+		} else { // Down
+			grid[y] = grid[y][:x] + "|" + grid[y][x+1:]
+		}
+	} else if dirs[0] == Left || dirs[1] == Left {
+		if dirs[0] == Right || dirs[1] == Right {
+			grid[y] = grid[y][:x] + "-" + grid[y][x+1:]
+		} else { // Down
+			grid[y] = grid[y][:x] + "7" + grid[y][x+1:]
+		}
+	} else { // Down and right
+		grid[y] = grid[y][:x] + "F" + grid[y][x+1:]
+	}
+
 	return dirs[0], dirs[1]
 }
 
-func Part1() {
-	lines, err := advent.ReadInput()
-	if err != nil {
-		panic(err)
-	}
-
+func GetDistances(grid []string) ([][]int, int) {
 	dist := [][]int{}
-	width := len(lines[0])
-	for range lines {
-		dist = append(dist, make([]int, width))
+	for _, line := range grid {
+		d := []int{}
+		for range line {
+			d = append(d, -1)
+		}
+		dist = append(dist, d)
 	}
 
 	sy := -1
 	sx := -1
-	for y, line := range lines {
+	for y, line := range grid {
 		for x, char := range line {
 			if char == 'S' {
 				sy = y
@@ -133,48 +150,58 @@ func Part1() {
 		}
 	}
 
-	dir1, dir2 := getConnectedDirs(lines, sy, sx)
+	dir1, dir2 := getConnectedDirs(grid, sy, sx)
 	y1 := sy
 	x1 := sx
 	y2 := sy
 	x2 := sx
 	maxDist := 0
-	for dist[y1][x1] == 0 && dist[y2][x2] == 0 {
+	for dist[y1][x1] == -1 && dist[y2][x2] == -1 {
 		dist[y1][x1] = maxDist
 		dist[y2][x2] = maxDist
 		maxDist += 1
 		switch dir1 {
 		case Up:
 			y1 -= 1
-			dir1 = fromBottom(lines, y1, x1)
+			dir1 = fromBottom(grid, y1, x1)
 		case Down:
 			y1 += 1
-			dir1 = fromTop(lines, y1, x1)
+			dir1 = fromTop(grid, y1, x1)
 		case Left:
 			x1 -= 1
-			dir1 = fromRight(lines, y1, x1)
+			dir1 = fromRight(grid, y1, x1)
 		case Right:
 			x1 += 1
-			dir1 = fromLeft(lines, y1, x1)
+			dir1 = fromLeft(grid, y1, x1)
 		}
 
 		switch dir2 {
 		case Up:
 			y2 -= 1
-			dir2 = fromBottom(lines, y2, x2)
+			dir2 = fromBottom(grid, y2, x2)
 		case Down:
 			y2 += 1
-			dir2 = fromTop(lines, y2, x2)
+			dir2 = fromTop(grid, y2, x2)
 		case Left:
 			x2 -= 1
-			dir2 = fromRight(lines, y2, x2)
+			dir2 = fromRight(grid, y2, x2)
 		case Right:
 			x2 += 1
-			dir2 = fromLeft(lines, y2, x2)
+			dir2 = fromLeft(grid, y2, x2)
 		}
 	}
 
-	fmt.Println(maxDist - 1)
+	return dist, maxDist - 1
+}
+
+func Part1() {
+	lines, err := advent.ReadInput()
+	if err != nil {
+		panic(err)
+	}
+
+	_, maxDist := GetDistances(lines)
+	fmt.Println(maxDist)
 }
 
 func main() {
--- day10/part2.go
+++ day10/part2.go
@@ -12,5 +12,18 @@ func Part2() {
 		panic(err)
 	}
 
-	fmt.Println(lines[0])
+	total := 0
+	dist, _ := GetDistances(lines)
+	for y, line := range lines {
+		inside := false
+		for x, char := range line {
+			if dist[y][x] != -1 && (char == 'L' || char == 'J' || char == '|') {
+				inside = !inside
+			} else if dist[y][x] == -1 && inside {
+				total += 1
+			}
+		}
+	}
+
+	fmt.Println(total)
 }
